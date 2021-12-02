04e42e20e17398dd827ad6430ea5eeadbd428999
Kawtar El-alami <kel-alam@e2r2p12.1337.ma>
2021-12-02 16:40:52 +0100

    add_styling

--- my_app/src/pages/Signup.jsx
+++ my_app/src/pages/Signup.jsx
@@ -1,7 +1,63 @@
 import React from 'react'
 import * as Yup from "yup"
 import { useFormik} from "formik"
+import styled  from "styled-components"
 
+const ButtonSignup = styled.button`
+	background: #7B68EE;
+	border-radius: 5px;
+	border: none;
+	box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+	color: white;
+	margin: 9px;
+	padding: 0.25em 1em;
+	width: 90px;
+	height: 35px;
+`
+const Form = styled.form`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	min-height: 70vh;
+	width: 40vw;
+	background: rgba(255, 255, 255, 0.15);
+	box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+	backdrop-filter: blur(8.5px);
+	-webkit-backdrop-filter: blur(8.5px);
+	border-radius: 10px;
+	color: #ffffff;
+	text-transform: uppercase;
+	letter-spacing: 0.4rem;
+}
+`;
+
+const Input = styled.input`
+	background: rgba(255, 255, 255, 0.15);
+	box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+	border-radius: 20px;
+	width: 70%;
+	margin: 9px;
+	height: ;
+	padding: 1rem;
+	border: none;
+	outline: none;
+	color: #3c354e;
+	font-size: 1rem;
+	font-weight: bold;
+	&:focus {
+		display: inline-block;
+		box-shadow: 0 0 0 0.1rem #b9abe0;
+		backdrop-filter: blur(12rem);
+		border-radius: 1rem;
+	}
+	&::placeholder {
+		color: #b9abe099;
+		font-weight: 100;
+		font-size: 1rem;
+	}
+
+`
 export default function Signup() {
 	const formik = useFormik({
 	  initialValues: {
@@ -30,43 +86,43 @@ export default function Signup() {
 	  },
 	});
 	return (
-	  <form onSubmit={formik.handleSubmit}>
-		<label htmlFor="firstName">First Name</label>
-		<input
+	  <Form onSubmit={formik.handleSubmit}>
+		<Input
 		  id="firstName"
 		  name="firstName"
+		  placeholder="Enter your firstName"
 		  type="text"
 		  onChange={formik.handleChange}
 		  value={formik.values.firstName}
 		/>
 		{formik.errors.firstName ? <p>{formik.errors.firstName}</p> : null}
-		<label htmlFor="lastName">Last Name</label>
-		<inputtr
+		<Input
 		  id="lastName"
 		  name="lastName"
+		  placeholder="Enter your lastName"
 		  type="text"
 		  onChange={formik.handleChange}
 		  value={formik.values.lastName}
 		/>
 		{formik.errors.lastName ? <p>{formik.errors.lastName}</p> : null}
-		<label htmlFor="email">Email Address</label>
-		<input
+		<Input
 		  id="email"
 		  name="email"
 		  type="email"
+		  placeholder="Enter your email"
 		  onChange={formik.handleChange}
 		  value={formik.values.email}
 		/>
-		<label htmlFor="password">Password</label>
-		<input
+		<Input
 		  id="password-signup"
 		  name="password"
 		  type="password"
+		  placeholder="Enter your password"
 		  onChange={formik.handleChange}
 		  value={formik.values.password}
 		/>
-		<button type="submit">Sign Up</button>
-	  </form>
+		<ButtonSignup type="submit">Sign Up </ButtonSignup>
+	  </Form>
 	);
   };
 
